9c62cebe7ceb044f965eb160f0565686603ac8ba
oussama <o.essemlali@gmail.com>
2021-04-01 21:15:43 +0200

    solution incomplete

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+let elements = ['.a','.b','.c']; 
 function init() {
 
 animateDiv(".a");
@@ -42,46 +42,66 @@ function makeNewPosition(zdiv){
 function getPosition(zdiv){
 let top= $(zdiv).offset().top;
 let left= $(zdiv).offset().left;
-let bottom= $(zdiv).offset().top + $(zdiv).height();
-let right= $(zdiv).offset().left + $(zdiv).width();
+let bottom= $(zdiv).offset().top + $(zdiv).height()+41.42;
+let right= $(zdiv).offset().left + $(zdiv).width()+41.42;
 return [top,left,bottom,right]
 }
 
-function collision(div1,div2){
-    [t1,l1,b1,r1]=getPosition(div1);
-    [t2,l2,b2,r2]=getPosition(div2);
+function collision([t1,l1,b1,r1],[t2,l2,b2,r2]){
     if(b1<t2 || t1>b2 || r1<l2 || l1>r2) return false;
         return true;
 }
 
-setInterval(()=>{
-    if(collision('.a','.b')){
-    $("#cons").append("Une collision entre .a et .b a ete detectee<br/>");
-    updateScroll();}
-    if(collision('.a','.c')){
-    $("#cons").append("Une collision entre .a et .c a ete detectee<br/>");
-    updateScroll();}
-    if(collision('.a','.c')){
-    $("#cons").append("Une collision entre .b et .c a ete detectee<br/>");
-    updateScroll();}
-},1)
+// setInterval(()=>{
+//     if(collision('.a','.b')){
+//         $('.a').stop();
+//         $('.b').stop();
+//     $("#cons").append("Une collision entre .a et .b a ete detectee<br/>");
+//     updateScroll();}
+//     if(collision('.a','.c')){
+//         $('.a').stop();
+//         $('.c').stop();
+//     $("#cons").append("Une collision entre .a et .c a ete detectee<br/>");
+//     updateScroll();}
+//     if(collision('.b','.c')){
+//         $('.b').stop();
+//         $('.c').stop();
+//     $("#cons").append("Une collision entre .b et .c a ete detectee<br/>");
+//     updateScroll();}
+// },1000)
 
 
 
 function animateDiv(zdiv){
     var newq = makeNewPosition(zdiv);
+    [t1,l1,b1,r1]=[newq[0],newq[1],newq[0]+142.42,newq[1]+142.42];
+    console.log([t1,l1,b1,r1])
     var oldq = $(zdiv).offset();
     var speed = calcSpeed([oldq.top, oldq.left], newq);
-    $(zdiv).animate(
-	    { top: newq[0], left: newq[1] },
-	    {
-	      duration: speed,
-              step: function(){ addRotate(zdiv,1); /* hint... */ },
-              complete: function(){ animateDiv(zdiv); }
-	    }
-	    );
-    $("#cons").append(zdiv+" going to ["+newq[0]+","+newq[1]+"]<br/>");
-    updateScroll();
+    [elt1,elt2]=elements.filter(elt=>elt!=zdiv);
+    console.log([elt1,elt2]);
+    if(collision([t1,l1,b1,r1],getPosition(elt1))){
+        $(zdiv).stop();
+        $("#cons").append(`Une collision entre ${zdiv} et ${elt1} a ete evitee<br/>`);
+        updateScroll();
+    }else{if(collision([t1,l1,b1,r1],getPosition(elt2))){
+        $(zdiv).stop();
+        $("#cons").append(`Une collision entre ${zdiv} et ${elt2} a ete evitee<br/>`);
+        updateScroll();
+    }else{
+        $(zdiv).animate(
+            { top: newq[0], left: newq[1] },
+            {
+                duration: speed,
+                    step: function(){ addRotate(zdiv,1); /* hint... */ },
+                    complete: function(){ animateDiv(zdiv); }
+            }
+            );
+            $("#cons").append(zdiv+" going to ["+newq[0]+","+newq[1]+"]<br/>");
+            updateScroll();
+    }}
+
+
 };
 
 function calcSpeed(prev,next) {
